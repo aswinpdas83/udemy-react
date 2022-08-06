d06219c43489df87d6c4ca47ebb7c7c4ca39630a
aswin <aswinpdas83@gmail.com>
2022-08-06 18:18:48 +0530

    render the template created by react

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,1 +1,10 @@
 console.log("App.Js is Running!")
+
+// function check() {
+//     alert('done');
+// }
+
+// var template = <p>This is JSX  from app.js </p>
+var template = React.createElement("h1", null, "This is JSX  from app.js ");
+var appRoot = document.getElementById("app");
+ReactDOM.render(template, appRoot);
