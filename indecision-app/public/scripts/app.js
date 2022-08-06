ee8038024df05e2816a2d313182003a7dd4ea0d1
aswin <aswinpdas83@gmail.com>
2022-08-06 18:26:57 +0530

    added styles and pass the atributes in object

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -5,6 +5,6 @@ console.log("App.Js is Running!")
 // }
 
 // var template = <p>This is JSX  from app.js </p>
-var template = React.createElement("h1", null, "This is JSX  from app.js ");
+var template = React.createElement("h1", { id: "tag", name: 'aswin', class: 'blue-class' }, "This is JSX  from app.js ");
 var appRoot = document.getElementById("app");
 ReactDOM.render(template, appRoot);
