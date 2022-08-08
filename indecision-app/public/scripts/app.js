6dc26210dbc0a7df03de8c61283154528405ddc9
aswin <aswinpdas83@gmail.com>
2022-08-08 13:20:02 +0530

    some values setting throug variables and objects

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -2,22 +2,24 @@
 
 console.log("App.Js is Running!");
 
-// function check() {
-//     alert('done');
-// }
+// JSX -  JavaScript XML
 
+var book = {
+    title: "Wings of fire",
+    subTitle: "Wings of Fire (1999), is the autobiography of the Missile Man of India and President of India, Dr. A. P. J. Abdul Kalam."
+};
 var template = React.createElement(
     "div",
     null,
     React.createElement(
         "h1",
         null,
-        "Does this change "
+        book.title
     ),
     React.createElement(
         "p",
         null,
-        "this is a info"
+        book.subTitle
     ),
     React.createElement(
         "ol",
@@ -34,6 +36,35 @@ var template = React.createElement(
         )
     )
 );
+var user = {
+    name: "Aswin P Das",
+    age: 21,
+    location: "Kochi, Ernakulam, Kerala"
+    // var userName = 'Aswin P Das'
+    // var userAge = 20
+    // var userLocation = "Kochi, Ernakulam"
+};var templateTwo = React.createElement(
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        user.name.toUpperCase()
+    ),
+    React.createElement(
+        "p",
+        null,
+        "Age: ",
+        user.age
+    ),
+    React.createElement(
+        "p",
+        null,
+        " Location: ",
+        user.location
+    )
+);
+//create an another template var JSX expression as templateTwo
 
 var appRoot = document.getElementById("app");
 ReactDOM.render(template, appRoot);
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,19 +1,39 @@
 console.log("App.Js is Running!")
 
-// function check() {
-//     alert('done');
-// }
+// JSX -  JavaScript XML
 
+var book = {
+    title: "Wings of fire",
+    subTitle: "Wings of Fire (1999), is the autobiography of the Missile Man of India and President of India, Dr. A. P. J. Abdul Kalam."
+}
 var template = (
     <div>
-        <h1>Does this change </h1>
-        <p>this is a info</p>
+        <h1>{book.title}</h1>
+        <p>{book.subTitle}</p>
         <ol>
             <li>item one</li>
             <li>item two</li>
         </ol>
     </div>
 );
+var user = {
+    name: "Aswin P Das",
+    age: 21,
+    location: "Kochi, Ernakulam, Kerala"
+
+}
+// var userName = 'Aswin P Das'
+// var userAge = 20
+// var userLocation = "Kochi, Ernakulam"
+var templateTwo = (
+    <div>
+        <h1>{user.name.toUpperCase()}</h1>
+        <p>Age: {user.age}</p>
+        <p> Location: {user.location}</p>
+
+    </div>
+)
+//create an another template var JSX expression as templateTwo
 
 var appRoot = document.getElementById("app");
 ReactDOM.render(template, appRoot);
