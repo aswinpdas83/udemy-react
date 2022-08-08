2d8ef6b67f9220e7f82653caca13729101a42536
aswin <aswinpdas83@gmail.com>
2022-08-08 11:51:33 +0530

    instlled node  modules and other dependencies

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,10 +1,16 @@
-console.log("App.Js is Running!")
+"use strict";
+
+console.log("App.Js is Running!");
 
 // function check() {
 //     alert('done');
 // }
 
-// var template = <p>This is JSX  from app.js </p>
-var template = React.createElement("h1", { id: "tag", name: 'aswin', class: 'blue-class' }, "This is JSX  from app.js ");
+var template = React.createElement(
+  "h1",
+  null,
+  "Does this change "
+);
+
 var appRoot = document.getElementById("app");
 ReactDOM.render(template, appRoot);
