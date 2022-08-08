e235b167865c9631f0493ace6f633eadc429232e
aswin <aswinpdas83@gmail.com>
2022-08-08 16:16:39 +0530

    variable creation using let,const,var

---
+++ indecision-app/src/playground/es6-let-const.js
@@ -0,0 +1,11 @@
+var nameVar = "Aswin"
+var nameVar = "Aswin P Das"
+console.log('nameVar:', nameVar)
+
+let nameLet = "Arun"
+nameLet = "Arun Das"
+console.log('nameLet:', nameLet)
+
+const nameConst = "Roji"
+// nameConst = 'Roji Roy' cant assign a new value to const variable (read-only)
+console.log('nameConst:', nameConst)
