34ab37daa5b6bd6c357428c9381af66396de1a4b
aswin <aswinpdas83@gmail.com>
2022-08-08 16:00:04 +0530

    condition statements

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -4,12 +4,14 @@ console.log("App.Js is Running!")
 
 var book = {
     title: "Wings of fire",
-    subTitle: "Wings of Fire (1999), is the autobiography of the Missile Man of India and President of India, Dr. A. P. J. Abdul Kalam."
+    subTitle: "Wings of Fire (1999), is the autobiography of the Missile Man of India and President of India, Dr. A. P. J. Abdul Kalam.",
+    options: ["one", "two"]
 }
 var template = (
     <div>
         <h1>{book.title}</h1>
-        <p>{book.subTitle}</p>
+        {book.subTitle && <p>{book.subTitle}</p>}
+        <p>{book.options.length > 0 ? 'Here is your options' : 'No options'}</p>
         <ol>
             <li>item one</li>
             <li>item two</li>
@@ -18,7 +20,7 @@ var template = (
 );
 var user = {
     name: "Aswin P Das",
-    age: 21,
+    age: 18,
     location: "Kochi, Ernakulam"
 
 }
@@ -32,7 +34,7 @@ function getLocation(location) {
 var templateTwo = (
     <div>
         <h1>{user.name ? user.name.toUpperCase() : "Anonymous"}</h1>
-        <p>Age: {user.age}</p>
+        {user.age >= 18 && <p>Age: {user.age}</p>}
         {getLocation(user.location)}
 
     </div>
@@ -40,4 +42,4 @@ var templateTwo = (
 //create an another template var JSX expression as templateTwo
 
 var appRoot = document.getElementById("app");
-ReactDOM.render(templateTwo, appRoot);
+ReactDOM.render(template, appRoot);
