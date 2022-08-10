14296884b054514850acd9a2f9a276c590fd83fa
aswin <aswinpdas83@gmail.com>
2022-08-10 13:00:26 +0530

    variable access outside the scope: var,let,const

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -11,3 +11,11 @@ console.log('nameLet:', nameLet);
 var nameConst = "Roji";
 // nameConst = 'Roji Roy' cant assign a new value to const variable (read-only)
 console.log('nameConst:', nameConst);
+
+// block scoping 
+var fullName = "Aswin Das";
+if (fullName) {
+    var firstName = fullName.split(" ")[0]; // let and const will not allow to print the value out side the scope.
+    console.log("First name: ", firstName);
+}
+console.log("First name out side the if scope: ", firstName);
--- indecision-app/src/playground/es6-let-const.js
+++ indecision-app/src/playground/es6-let-const.js
@@ -9,3 +9,11 @@ console.log('nameLet:', nameLet)
 const nameConst = "Roji"
 // nameConst = 'Roji Roy' cant assign a new value to const variable (read-only)
 console.log('nameConst:', nameConst)
+
+// block scoping 
+var fullName = "Aswin Das";
+if (fullName) {
+    var firstName = fullName.split(" ")[0]; // let and const will not allow to print the value out side the scope.
+    console.log("First name: ", firstName);
+}
+console.log("First name out side the if scope: ", firstName);
