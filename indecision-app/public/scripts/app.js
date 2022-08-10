d9233b5dc8afbf45e9391c37723e1606fcc889f3
aswin <aswinpdas83@gmail.com>
2022-08-10 14:29:06 +0530

    mao and filter and arrow functions

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -18,4 +18,26 @@ if (fullName) {
     var firstName = fullName.split(" ")[0]; // let and const will not allow to print the value out side the scope.
     console.log("First name: ", firstName);
 }
-console.log("First name out side the if scope: ", firstName);
+console.log("First name out side the if scope:", firstName);
+
+var getSqur = function getSqur(x) {
+    return Math.pow(x, 2);
+};
+
+console.log("\n Answer is : ", getSqur(4));
+
+var words = ['apple', 'ball', 'cat', 'duck'];
+// function upperConvert(word) {
+//     return word.toUpperCase();
+// }
+
+// const arrowConvert = (word) => word.toUpperCase();
+
+
+var wordsUpper = words.map(function (word) {
+    return word.toUpperCase();
+});
+
+// const filterPredicate = (word) => word.includes('a');
+// const filtered = words.filter(filterPredicate);
+console.log(wordsUpper);
