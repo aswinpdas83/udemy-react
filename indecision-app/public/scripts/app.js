a35e61c6f7f0498b8b5fd893f33f3f28e502e782
aswin <aswinpdas83@gmail.com>
2022-08-10 17:13:51 +0530

    challege : udemy tutor Andrew

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,14 +1,79 @@
 "use strict";
 
-var user = {
-    name: "Aswin",
-    location: ["Mavelikara", 'Kochi', 'Kayamkulam'],
-    printPlacedLived: function printPlacedLived() {
-        var _this = this;
+console.log("App.Js is Running!");
 
-        this.location.map(function (city) {
-            return console.log(_this.name, "has lived in", city);
-        });
-    }
+// JSX -  JavaScript XML
+
+var book = {
+    title: "Wings of fire",
+    subTitle: "Wings of Fire (1999), is the autobiography of the Missile Man of India and President of India, Dr. A. P. J. Abdul Kalam.",
+    options: ["one", "two"]
 };
-user.printPlacedLived();
+var template = React.createElement(
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        book.title
+    ),
+    book.subTitle && React.createElement(
+        "p",
+        null,
+        book.subTitle
+    ),
+    React.createElement(
+        "p",
+        null,
+        book.options.length > 0 ? 'Here is your options' : 'No options'
+    ),
+    React.createElement(
+        "ol",
+        null,
+        React.createElement(
+            "li",
+            null,
+            "item one"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "item two"
+        )
+    )
+);
+var user = {
+    name: "Aswin P Das",
+    age: 18,
+    location: "Kochi, Ernakulam"
+    // var userName = 'Aswin P Das'
+    // var userAge = 20
+    // var userLocation = "Kochi, Ernakulam"
+};function getLocation(location) {
+    if (location) return React.createElement(
+        "p",
+        null,
+        "Location: ",
+        location
+    );
+}
+var templateTwo = React.createElement(
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        user.name ? user.name.toUpperCase() : "Anonymous"
+    ),
+    user.age >= 18 && React.createElement(
+        "p",
+        null,
+        "Age: ",
+        user.age
+    ),
+    getLocation(user.location)
+);
+//create an another template var JSX expression as templateTwo
+
+var appRoot = document.getElementById("app");
+ReactDOM.render(template, appRoot);
--- indecision-app/src/playground/es6-arrow-function-2.js
+++ indecision-app/src/playground/es6-arrow-function-2.js
@@ -3,7 +3,17 @@ const user = {
     location: ["Mavelikara", 'Kochi', 'Kayamkulam'],
     printPlacedLived: function () {
         this.location.map(city => console.log(this.name, "has lived in", city));
-
     }
 }
 user.printPlacedLived()
+
+const multiplier = {
+    numbers: [10, 20, 30, 40, 50],
+    multiplyBy: 2,
+    multiply() {
+        return this.numbers.map(num => num * this.multiplyBy)
+    }
+}
+console.log(multiplier.multiply())
+
+const add = (a, b) => a + b;
