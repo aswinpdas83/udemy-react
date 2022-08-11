447cc59334fce20f946f09cee42acd68f4608e4c
aswin <aswinpdas83@gmail.com>
2022-08-11 17:47:52 +0530

    es6-classs and overrideing the class

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,96 +1,95 @@
-"use strict";
+'use strict';
 
-console.log("App.Js is Running!");
+var _get = function get(object, property, receiver) { if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { return get(parent, property, receiver); } } else if ("value" in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } };
 
-// JSX -  JavaScript XML
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-var app = {
-    title: "Wings of fire",
-    subTitle: "Wings of Fire (1999), is the autobiography of the Missile Man of India and President of India, Dr. A. P. J. Abdul Kalam.",
-    options: []
-};
-var onFormSumbit = function onFormSumbit(e) {
-    e.preventDefault();
-    var option = e.target.elements.options.value;
-    if (option) {
-        app.options.push(option);
-        e.target.elements.options.value = '';
-        renderFunction();
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Person = function () {
+    function Person() {
+        var name = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'Anonymous';
+        var age = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
+
+        _classCallCheck(this, Person);
+
+        this.name = name;
+        this.age = age;
     }
-};
 
-var onRemoveAll = function onRemoveAll() {
-    app.options = [];
-    renderFunction();
-};
+    _createClass(Person, [{
+        key: 'getGreeting',
+        value: function getGreeting() {
+            return 'Hi. I am ' + this.name + '!';
+        }
+    }, {
+        key: 'getDescription',
+        value: function getDescription() {
+            return this.name + ' has ' + this.age + ' year(s) old.';
+        }
+    }]);
 
-var onMakeDecision = function onMakeDecision() {
-    var randomNum = Math.floor(Math.random() * app.options.length);
-    var option = app.options[randomNum];
-    alert(option);
-    //option ? alert(`Your option is: ${option}`) : undefined
-};
-// const renderList = () => app.options.map((item, index) => <li key={item}>{item}</li>)
+    return Person;
+}();
 
-var appRoot = document.getElementById("app");
+var Student = function (_Person) {
+    _inherits(Student, _Person);
 
-var renderFunction = function renderFunction() {
-    var template = React.createElement(
-        "div",
-        null,
-        React.createElement(
-            "h1",
-            null,
-            app.title
-        ),
-        app.subTitle && React.createElement(
-            "p",
-            null,
-            app.subTitle
-        ),
-        React.createElement(
-            "p",
-            null,
-            app.options.length > 0 ? 'Here is your options' : 'No options'
-        ),
-        React.createElement(
-            "p",
-            null,
-            app.options.length
-        ),
-        React.createElement(
-            "button",
-            { disabled: app.options.length === 0, onClick: onMakeDecision },
-            "What should I do?"
-        ),
-        React.createElement(
-            "button",
-            { onClick: onRemoveAll },
-            "Remove All"
-        ),
-        React.createElement(
-            "ol",
-            null,
-            app.options.map(function (item, index) {
-                return React.createElement(
-                    "li",
-                    { key: index },
-                    item
-                );
-            })
-        ),
-        React.createElement(
-            "form",
-            { onSubmit: onFormSumbit },
-            React.createElement("input", { type: "text", name: "options", placeholder: "Enter the value here" }),
-            React.createElement(
-                "button",
-                null,
-                "Add options"
-            )
-        )
-    );
-    ReactDOM.render(template, appRoot);
-};
+    function Student(name, age, major) {
+        _classCallCheck(this, Student);
 
-renderFunction();
+        var _this = _possibleConstructorReturn(this, (Student.__proto__ || Object.getPrototypeOf(Student)).call(this, name, age));
+
+        _this.major = major;
+        return _this;
+    }
+
+    _createClass(Student, [{
+        key: 'hasMajor',
+        value: function hasMajor() {
+            return !!this.major;
+        }
+    }, {
+        key: 'getDescription',
+        value: function getDescription() {
+            var description = _get(Student.prototype.__proto__ || Object.getPrototypeOf(Student.prototype), 'getDescription', this).call(this);
+            if (this.major) {
+                description += ' Their major is ' + this.major + '.';
+            }
+            return description;
+        }
+    }]);
+
+    return Student;
+}(Person);
+
+var Traveler = function (_Person2) {
+    _inherits(Traveler, _Person2);
+
+    function Traveler(name, age, homeLocation) {
+        _classCallCheck(this, Traveler);
+
+        var _this2 = _possibleConstructorReturn(this, (Traveler.__proto__ || Object.getPrototypeOf(Traveler)).call(this, name, age));
+
+        _this2.homeLocation = homeLocation;
+        return _this2;
+    }
+
+    return Traveler;
+}(Person);
+
+var me = new Traveler('Aswin P Das', 21, 'Kochi');
+console.log(me);
+// console.log(me.getGreeting())
+// console.log(me.getDescription())
+// console.log(me.hasMajor())
+
+var other = new Traveler();
+console.log(other);
+// console.log(other.getGreeting())
+// console.log(other.getDescription())
+// console.log(other.hasMajor())
