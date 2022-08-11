caf6a0acec2ae6dfd944507971d46d377f078434
aswin <aswinpdas83@gmail.com>
2022-08-11 14:06:11 +0530

    babal outfile of #built-it-visible.js

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,96 +1,23 @@
-"use strict";
+'use strict';
 
-console.log("App.Js is Running!");
+var buttonName = 'Show details';
 
-// JSX -  JavaScript XML
-
-var book = {
-    title: "Wings of fire",
-    subTitle: "Wings of Fire (1999), is the autobiography of the Missile Man of India and President of India, Dr. A. P. J. Abdul Kalam.",
-    options: []
-};
-var onFormSumbit = function onFormSumbit(e) {
-    e.preventDefault();
-    var option = e.target.elements.options.value;
-    if (option) {
-        book.options.push(option);
-        e.target.elements.options.value = '';
-        renderFunction();
-    }
+var onClickToggle = function onClickToggle() {
+    render();
 };
 
-var onRemoveAll = function onRemoveAll() {
-    book.options = [];
-    renderFunction();
-};
-
-var onMakeDecision = function onMakeDecision() {
-    var randomNum = Math.floor(Math.random() * book.options.length);
-    var option = book.options[randomNum];
-    alert(option);
-    // option ? alert("Your option is:", option) : undefined
-};
-// const renderList = () => book.options.map(item => <li key={item}>{item}</li>)
-
-var appRoot = document.getElementById("app");
-
-var renderFunction = function renderFunction() {
-    var template = React.createElement(
-        "div",
+var render = function render() {
+    var tmlt = React.createElement(
+        'div',
         null,
         React.createElement(
-            "h1",
+            'h1',
             null,
-            book.title
+            'Visiblity Toggle'
         ),
-        book.subTitle && React.createElement(
-            "p",
-            null,
-            book.subTitle
-        ),
-        React.createElement(
-            "p",
-            null,
-            book.options.length > 0 ? 'Here is your options' : 'No options'
-        ),
-        React.createElement(
-            "p",
-            null,
-            book.options.length
-        ),
-        React.createElement(
-            "button",
-            { disabled: book.options.length === 0, onClick: onMakeDecision },
-            "What should I do?"
-        ),
-        React.createElement(
-            "button",
-            { onClick: onRemoveAll },
-            "Remove All"
-        ),
-        React.createElement(
-            "ol",
-            null,
-            book.options.map(function (item, index) {
-                return React.createElement(
-                    "li",
-                    { key: index },
-                    item
-                );
-            })
-        ),
-        React.createElement(
-            "form",
-            { onSubmit: onFormSumbit },
-            React.createElement("input", { type: "text", name: "options", placeholder: "Enter the value here" }),
-            React.createElement(
-                "button",
-                null,
-                "Add options"
-            )
-        )
+        React.createElement('button', { onClick: onClickToggle, value: buttonName }),
+        React.createElement('p', null)
     );
-    ReactDOM.render(template, appRoot);
+    ReactDOM.render(tmlt, document.getElementById('app'));
 };
-
-renderFunction();
+render();
