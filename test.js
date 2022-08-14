0944c99b9a04dc95b01dbe06e292766e2edc011c
aswin <aswinpdas83@gmail.com>
2022-08-14 15:50:01 +0530

    Initial commit to create the react app

--- test.js
+++ test.js
@@ -1,1 +1,6 @@
-<script src="https://unpkg.com/react@15/dist/react.js"></script>
+// <script src="https://unpkg.com/react@15/dist/react.js"></script>
+
+var arr = [3, 4, 5]
+arr.foo = 'hello'
+arr = { ...arr }
+console.log([arr])
