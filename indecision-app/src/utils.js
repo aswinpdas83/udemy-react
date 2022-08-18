25a328fee28eee6bdfb9624a644a64c2c13cba2c
aswin <aswinpdas83@gmail.com>
2022-08-18 13:30:27 +0530

    removed files

--- indecision-app/src/utils.js
+++
@@ -1,8 +0,0 @@
-console.log('utils.js running.');
-
-export const square = (x) => x ** 2;
-export const add = (a, b) => a + b;
-const subtract = (a, b) => a - b;
-export default subtract;
-
-//export { square, add, subtract as default };
