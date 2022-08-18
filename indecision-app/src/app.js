bbdb01697687473a5839d855d07ff11743dd94c6
aswin <aswinpdas83@gmail.com>
2022-08-18 12:50:33 +0530

    re-installed babel-loader@7.1.1 &babel-core@6.25.0

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -2,5 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 
-const template = React.createElement('p', {}, 'React testing.')
+const template = <p>Welcome to  react</p>;
 ReactDOM.render(template, document.getElementById('app'));
--- indecision-app/webpack.config.js
+++ indecision-app/webpack.config.js
@@ -4,7 +4,13 @@ module.exports = {
     entry: './src/app.js',
     output: {
         path: path.join(__dirname, 'public'),
-        filename: 'bundle.js',
-
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            loader: 'babel-loader',
+            test: /\.js$/,
+            exclude: /node_modules/
+        }]
     }
 };
